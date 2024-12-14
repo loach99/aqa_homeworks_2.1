a944fd18a70b4ff38bb37ee2f1e8a9086dcc5d69
Константин Вьюнков <113593006+loach99@users.noreply.github.com>
2024-12-14 11:37:56 +0300

    Update OrderTest.java

--- src/test/java/OrderTest.java
+++ src/test/java/OrderTest.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.junit.jupiter.api.Assertions;
 
 import java.util.List;
 
